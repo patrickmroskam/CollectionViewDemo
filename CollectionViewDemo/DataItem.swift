6e8aa7674cb8dae70473af9f9c2c732729e1b93a
Patrick M. Roskam <pat.roskam@gmail.com>
2018-03-25 19:35:32 -0700

    tested after add DataItem

---
+++ CollectionViewDemo/DataItem.swift
@@ -0,0 +1,35 @@
+//
+//  DataItem.swift
+//  CollectionViewDemo
+//
+//  Created by Patrick Roskam on 3/25/18.
+//  Copyright © 2018 PatrickMRoskam. All rights reserved.
+//
+
+import UIKit
+
+class DataItem {
+    var title: String
+    var kind: Kind
+    var imageName: String
+    
+    init(title: String, kind: Kind, imageName: String) {
+        self.title = title
+        self.kind = kind
+        self.imageName = imageName
+    }
+}
+
+enum Kind: Int {
+    case Plant
+    case Animal
+    
+    func description() -> String {
+        switch self {
+        case .Plant:
+            return "Plants"
+        case .Animal:
+            return "Animals"
+        }
+    }
+}
